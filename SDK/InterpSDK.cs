48a82d49f1121434c831269a9d604df744a477b2
johnseed <48501700+johnseed@users.noreply.github.com>
2022-06-16 11:36:47 +0000

    js dotnet invoke

---
+++ SDK/InterpSDK.cs
@@ -0,0 +1,35 @@
+using Dapr.Client;
+using Grpc.Net.Client;
+using System.Text.Json.Serialization;
+
+public static class InterpSDK
+{
+    public async static Task<InterpResult> Interp(List<int> lon, List<int> lat)
+    {
+        var daprClient = new DaprClientBuilder().UseGrpcChannelOptions(new GrpcChannelOptions()
+        {
+            MaxReceiveMessageSize = 2047 * 1024 * 1024,
+            MaxSendMessageSize = 2047 * 1024 * 1024
+        }).Build();
+        string request = "interp";
+        var data = new
+        {
+            lon = lon,
+            lat = lat
+        };
+        InterpResult? result = await daprClient.InvokeMethodAsync<object, InterpResult>("gda-algorithm-interp", request, data);
+        return result;
+    }
+}
+
+public class InterpResult
+{
+    [JsonPropertyName("lon_grid")]
+    public List<List<double>> LonGrid { get; set; }
+
+    [JsonPropertyName("lat_grid")]
+    public List<List<double>> LatGrid { get; set; }
+
+    [JsonPropertyName("tm_grid")]
+    public List<List<double>> TmGrid { get; set; }
+}
---
+++ SDK/Store.cs
@@ -0,0 +1,5 @@
+using Microsoft.JSInterop;
+public static class Store
+{
+    public static IJSRuntime JS { get; set; }
+}
